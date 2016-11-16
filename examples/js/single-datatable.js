66ec74af49308d788695f4b563da3794a3f19ef2
Omar <omar.ramirezsudati@gmail.com>
2016-11-16 17:15:31 -0300

    Doing an example

---
+++ examples/js/single-datatable.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+
+    $('#maintable').DataTable( {
+		iDisplayLength: 20,
+		aaData: data,
+		columns: [
+            { title: "Name" },
+            { title: "Position" },
+            { title: "Office" },
+            { title: "Extn." },
+            { title: "Start date" },
+            { title: "Salary" }
+        ],
+		ordering:true,
+		//scrollY:'100%',
+		//scrollX: true,
+		//scrollCollapse:true,
+		stateSave:false
+	});
+} );
